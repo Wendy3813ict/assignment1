2b0d9da88c0ee22b1c27aa2488f40e5f6d699e6e
Wendy3813ict <ying.yu2@griffithuni.edu.au>
2019-09-05 00:33:32 +1000

    done the directory setting and interface setting start to set the router

--- assignment1/src/app/app.module.ts
+++ assignment1/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {LoginComponent} from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './auth/admin/admin.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { UserComponent } from './auth/user/user.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    LogoutComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- assignment1/src/app/auth/login/login.component.ts
+++ assignment1/src/app/auth/login/login.component.ts
@@ -1,12 +1,15 @@
-import {Component} from "@angular/core";
-import {Injectable} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: './login.component.html'
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
+export class LoginComponent implements OnInit {
 
-export class LoginComponent {
-    
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
